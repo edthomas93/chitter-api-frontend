a74b98d4def169020cacd46252465694085bf873
edthomas93 <et1270@my.bristol.ac.uk>
2018-12-08 17:27:57 +0000

    formatted peeps

--- peeps.js
+++ peeps.js
@@ -5,7 +5,7 @@ peeps.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps');
 peeps.onload = function () {
   var data = JSON.parse(peeps.responseText);
   renderHTML(data);
-}
+};
 
 peeps.send();
 
@@ -13,8 +13,8 @@ function renderHTML(data) {
   var HTMLstring = "";
 
   for(var i=0; i<data.length; i++) {
-    HTMLstring += "<p>" + data[i].user.handle + ": " + data[i].body + "</p>"
+    HTMLstring += `<p> ${data[i].user.handle}: "${data[i].body}" @${data[i].created_at.slice(11, 16)} on ${data[i].created_at.slice(0, 10)}</p>`
   }
 
   peepsContainer.insertAdjacentHTML('beforeEnd', HTMLstring);
-}
+};
